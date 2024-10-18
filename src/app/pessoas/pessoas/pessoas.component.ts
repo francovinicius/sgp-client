6da5a7f57fe85ca98eef46c6e00e95f569d964a9
francovinicius <duartevini.dev@gmail.com>
2024-10-18 15:59:44 -0300

    Add fun filtrar clients

--- src/app/pessoas/pessoas/pessoas.component.ts
+++ src/app/pessoas/pessoas/pessoas.component.ts
@@ -23,6 +23,8 @@ export class PessoasComponent {
   //objeto cliente
   cliente = new Cliente();
 
+  //novo obj
+  clientesFiltrados: Cliente[] = []; // Lista exibida com base no filtro
 
   //JSON de pessoas
   clientes: Cliente[] = [];
@@ -31,32 +33,25 @@ export class PessoasComponent {
   clienteSelecionado: Cliente = new Cliente();
 
 
-  //metodo para editar cliente
-
-  deletar(id: number): void {
-    const confirmacao = confirm('Tem certeza que deseja deletar este cliente?');
-    if (confirmacao) {
-      this.servico.remover(id).subscribe({
-        next: () => {
-          // Atualizar a lista local após a exclusão no backend
-          this.clientes = this.clientes.filter(c => c.id !== id);
-          console.log('Cliente deletado com sucesso!');
-        },
-        error: (err) => {
-          console.error('Erro ao deletar cliente', err);
-        }
-      });
-    }
-  }
-
-
-
   constructor(private servico: ClienteService, private router: Router) { }
+
+  ngOnInit() {
+    this.selecionar();
+  }
 
   // metodo seleção pessoas
   selecionar(): void {
-    this.servico.selecionar()
-      .subscribe(retorno => this.clientes = retorno);
+    this.servico.selecionar().subscribe(retorno => {
+      this.clientes = retorno;
+      this.clientesFiltrados = retorno; // Inicialmente, exibe todos
+    });
+  }
+
+  filtrarClientes(event: Event): void {
+    const valor = (event.target as HTMLInputElement).value.toLowerCase();
+    this.clientesFiltrados = this.clientes.filter(cliente =>
+      cliente.nome.toLowerCase().includes(valor)
+    );
   }
 
   selecionarCliente(cliente: Cliente): void {
@@ -76,8 +71,21 @@ export class PessoasComponent {
     });
   }
 
-  ngOnInit() {
-    this.selecionar();
+//metodo deletar cliente
+  deletar(id: number): void {
+    const confirmacao = confirm('Tem certeza que deseja deletar este cliente?');
+    if (confirmacao) {
+      this.servico.remover(id).subscribe({
+        next: () => {
+          // Atualizar a lista local após a exclusão no backend
+          this.clientes = this.clientes.filter(c => c.id !== id);
+          console.log('Cliente deletado com sucesso!');
+        },
+        error: (err) => {
+          console.error('Erro ao deletar cliente', err);
+        }
+      });
+    }
   }
 
 }
